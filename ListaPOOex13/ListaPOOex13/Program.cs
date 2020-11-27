8fb2130d17373fb04aded055e0f8a1ca18a858e8
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:05:45 -0300

    Add files via upload

---
+++ ListaPOOex13/ListaPOOex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ListaPOOex13
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triang;
+
+            triang = new Triangulo();
+
+            Console.Write("Informe o 1° valor: ");
+            triang.setValor1(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o 2° valor: ");
+            triang.setValor2(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o 3° valor: ");
+            triang.setValor3(double.Parse(Console.ReadLine()));
+
+            triang.verificarTring();
+
+
+        }
+    }
+}
---
+++ ListaPOOex13/ListaPOOex13/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex13
+{
+    class Triangulo
+    {
+        private double A;
+        private double B;
+        private double C;
+
+        public void setValor1(double p)
+        {
+            A = p;
+        }
+
+        public void setValor2(double p)
+        {
+            B = p;
+        }
+
+        public void setValor3(double p)
+        {
+            C = p;
+        }
+
+        public void verificarTring()
+        {
+            if (A * A + B * B == C * C || B * B + C * C == A * A || A * A + C * C == B * B)
+            {
+                Console.WriteLine("É um triangulo retangulo");
+            }
+            else
+            {
+                Console.WriteLine("Não é um triangulo retangulo");
+            }
+        }
+    }
+}
